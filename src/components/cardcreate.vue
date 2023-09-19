ccd221a82133c05c7ac7844589fc03feb6f7238b
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-09-19 14:58:23 -0300

    FEAT projeto idealizado

---
+++ src/components/cardcreate.vue
@@ -0,0 +1,62 @@
+<template>
+    <h1>teste</h1>
+</template>
+
+<script>
+    export default {
+        name: 'CardCreate',
+    }
+</script>
+
+<style lang="sass">
+$color1: #131213
+$color2: #2C2C2C
+$white: #F5F5F5
+
+//card
+$pink: #DD45C5
+$bluec: #354668
+$cardd: $bluec, $pink, $bluec
+
+@mixin gradient($direction, $cardd) 
+  background: -webkit-linear-gradient($direction, $cardd)
+
+.container-card
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+    width: 50%
+    margin-top: 4rem
+    margin-left: 30p
+    color: $white
+    .card
+        @include gradient(left, $cardd)
+        width: 400px
+        height: 250px
+        border-radius: 30px
+        .start-card-info
+            .balance
+                font-size: 40px
+                color: $white
+                padding-left: 30px
+                p
+                    font-size: 20px
+                    margin: 0
+                    margin-top: 2rem
+                h1
+                    margin: 0
+            .card-info
+                display: flex
+                align-items: flex-end
+                padding-left: 30px
+                gap: 1.5rem
+                margin-top: 4.2rem
+                .card-flag
+                    svg
+                        color: $white
+                        width: 50px
+                        height: 36px
+                .card-name
+                    color: $white
+</style>
---
+++ src/components/cardpage.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="container-project">
+        <div class="container-card">
+            <cardstart/>
+        </div>
+
+        <div class="container-image">
+            <img src="../../public/img-cred-card.png" alt="">
+        </div>
+    </section>
+</template>
+
+<script>
+import cardstart from '../components/cardstart.vue'
+
+    export default {
+        name: 'CardPage',
+        components: {
+            cardstart,
+
+        }
+    }
+</script>
+
+<style lang="sass">
+$color1: #131213
+$color2: #2C2C2C
+$white: #F5F5F5
+
+//card
+$pink: #DD45C5
+$bluec: #354668
+$cardd: $bluec, $pink, $bluec
+
+@mixin gradient($direction, $cardd) 
+  background: -webkit-linear-gradient($direction, $cardd)
+
+body
+    background-color: $color1
+    margin: 0
+    padding: 0
+    box-sizing: border-box
+    font-family: Arial, Helvetica, sans-serif
+
+    .container-project
+        display: flex
+        justify-content: center
+        margin: 0 auto
+        height: 700px
+        
+        .container-image
+            width: 50%
+            display: flex
+            justify-content: center
+            align-items: center
+            img
+                margin: 0 auto
+                width: 500px
+
+</style>
